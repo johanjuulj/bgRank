c6a255afaaee1b4f96c7733dbe718e820571f4d3
johanjuulj <johanjuulj@gmail.com>
2023-03-26 22:24:15 +0200

    add player

---
+++ bgRank/Models/Player.cs
@@ -0,0 +1,10 @@
+﻿namespace bgRank.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Score { get; set; }
+    }
+}
