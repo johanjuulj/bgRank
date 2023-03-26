29d6df4abf2909df88557319deb1cc84582a2f6d
johanjuulj <johanjuulj@gmail.com>
2023-03-26 22:27:00 +0200

    added result model

---
+++ bgRank/Models/Result.cs
@@ -0,0 +1,12 @@
+﻿namespace bgRank.Models
+{
+    public class Result
+    {
+        public int Id { get; set; }
+        public List<Player> Players { get; set; }
+
+        public BoardGame game { get; set; }
+
+        public Result() { }
+    }
+}
