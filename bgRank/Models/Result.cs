9ea6d137496819535e5431bfef6b89d87ad35b4b
johanjuulj <johanjuulj@gmail.com>
2023-03-27 22:28:24 +0200

    updating result

--- bgRank/Models/Result.cs
+++ bgRank/Models/Result.cs
@@ -5,6 +5,8 @@
         public int Id { get; set; }
         public List<Player> Players { get; set; }
 
+        public int Time { get; set; }
+
         public BoardGame game { get; set; }
 
         public Result() { }
