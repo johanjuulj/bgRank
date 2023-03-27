a7abebb968509c2aefefc5657ad85cc697888118
johanjuulj <johanjuulj@gmail.com>
2023-03-27 22:30:19 +0200

    updated boardgame model to include complexity

--- bgRank/Models/BoardGame.cs
+++ bgRank/Models/BoardGame.cs
@@ -5,7 +5,9 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        
+
+        public int Complexity { get; set; }
+
         public BoardGame() { }
         
     }
